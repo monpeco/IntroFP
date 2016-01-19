e4cfe52176a200e8fcabe0b2abef83ea47345e3f
monpeco <monpeco@gmail.com>
2016-01-18 22:23:03 -0300

    Add function and testing the Curring Notation Added: add, add', zeroto and mult
    * Lecture 2 Types and Classes

--- functions.hs
+++ functions.hs
@@ -1,3 +1,4 @@
+--Basic functions
 double x = x + x
 
 qua x = double (double x)
@@ -8,5 +9,30 @@ suma n = sum [1..n]
 
 average ns = sum ns `div` length ns
 
+average' ns = div (sum ns) (length ns)
 
+--no parameter function
+fun_c = b + c
+	where 
+		b = 1
+		c = 8
+--one parameter function
+fun_d b = b + c
+	where 
+		c = 8
+{-
+curly brackets comment
+used for multi line comments
+-}		
+fun_e b = b + c + d
+	where 
+		{c = 10;
+		d = 10}		
+		
+add x y = x + y	
+	
+add' (x, y) = x + y	
 
+zeroto x = [0..x]
+
+mult x y z = x*y*z
