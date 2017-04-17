58c558808079da8c7192e099dc23ac411afe5d5e
monpeco <monpeco@gmail.com>
2017-04-17 12:55:25 +0000

    Add math functions

--- db/tutorial.hs
+++ db/tutorial.hs
@@ -43,3 +43,44 @@ bigFloat = 3.99999999999 + 0.00000000005
 always5 :: Int
 always5 = 5
 
+-- ---------- MATH ----------
+-- 9
+sumOfVals = sum [1..1000]
+{-
+*Main> sumOfVals 
+500500
+-}
+
+addEx = 5 + 4
+subEx = 5 - 4
+divEx = 5 / 4
+multEx = 5 * 4
+
+-- 10
+--mod is a prefix operator
+modEx = mod 5 4
+
+--with back ticks can be used as infix operator
+modEx2 = 5 `mod` 4
+
+-- negative numbers must be surrounded with parenthesess
+negNum = 4 + (-5)
+
+-- If you define an Int you must use fromIntegral to use it with sqrt
+-- :t sqrt shows that it returns a floating point number
+num9 = 9 :: Int
+sqrtOf9 = sqrt(fromIntegral num9)
+
+-- Built in math functions
+piVal = pi
+ePow9 = exp 9
+logOf9 = log 9
+squared9 = 9 ** 2
+truncateVal = truncate 9.999
+roundVal = round 9.999
+ceilingVal = ceiling 9.999
+floorVal = floor 9.999
+
+-- Also sin, cos, tan, asin, atan, acos, sinh, tanh, cosh, asinh, atanh, acosh
+
+
