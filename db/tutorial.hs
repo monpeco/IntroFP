8b1ca8f8748c75b3e878e18590f8edae8a29044a
monpeco <monpeco@gmail.com>
2017-04-21 16:52:01 +0000

    Add tuples (store, retrieve and combine data)

--- db/tutorial.hs
+++ db/tutorial.hs
@@ -229,3 +229,26 @@ power3ListDiv9 = [3^n | n <- [1..10], 3^n `mod` 9 == 0]
 -- Generate a multiplication table by multiplying x * y where y has the values
 -- 1 through 10 and where x does as well
 multTable = [[x*y | x <- [1..10]] | y <- [1..10] ]
+
+
+-- ---------- TUPLES ----------
+-- Stores list of multiple data types, but has a fixed size
+
+randTuple = (1,"Random tuple")
+
+-- A tuple pair stores 2 values
+bobSmith = ("Bob Smith",52)
+
+-- Get the first value
+bobsName = fst bobSmith
+ 
+-- Get the second value
+bobsAge = snd bobSmith
+
+-- zip can combine values into tuple pairs 
+names = ["Bob","Mary","Tom"]
+addresses = ["123 Main","234 North","567 South"]
+ 
+namesNAddress = zip names addresses 
+-- [("Bob","123 Main"),("Mary","234 North"),("Tom","567 South")]
+
